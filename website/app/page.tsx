08b968d134f31242656ebdfd8c1f1719c7e1b798
Wyatt Morgan <iamtheyy@gmail.com>
2025-11-07 17:14:19 -0600

    Setup app on home pc and made initial commit

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -6,7 +6,9 @@ export default function Home() {
         Hello World!
       </h1>
       <p className="text-lg text-gray-600">
-        This is what the main page will look like. Probably the login page maybe?
+        This is what the main page will look like. Probably the login page maybe?<br />
+
+        I (Wyatt) got this all setup on my home pc and will on my laptop, but I don't want to touch this page until we have a conversation on where we want to go with it
       </p>
     </main>
   );
