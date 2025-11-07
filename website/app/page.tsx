3fd2becfe19daabf14e4f9930bfc1450d8a3a6b1
Corey Unruh <113541726+CUnruh641@users.noreply.github.com>
2025-11-07 11:55:23 -0600

    Create next.js project for web development

---
+++ website/app/page.tsx
@@ -0,0 +1,13 @@
+//Main landing page. Make new folders in /app, then add a page.tsx in those folders to create new pages.
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">
+        Hello World!
+      </h1>
+      <p className="text-lg text-gray-600">
+        This is what the main page will look like. Probably the login page maybe?
+      </p>
+    </main>
+  );
+}
