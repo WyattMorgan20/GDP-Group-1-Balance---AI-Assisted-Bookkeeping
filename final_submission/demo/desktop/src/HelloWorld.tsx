f8767bbbb3651718c5845f9ea62d28e9e06284f7
grahowe <78877586+grahowe@users.noreply.github.com>
2026-01-28 10:42:19 -0600

    Added HelloWorld login page

---
+++ final_submission/demo/desktop/src/HelloWorld.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/core";
+
+
+function HelloWorld() {
+    
+    return (
+        <h1>Hello World!</h1>
+    );
+
+}
+
+export default HelloWorld
